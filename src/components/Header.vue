42c1d6001b3fc355d2099e91c0342c8e2e100afe
qikke <qikke1995@126.com>
2018-08-12 17:59:58 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="topNavBar">
+    <div class="container clearfix">
+      <a class="logo"><img src="https://o4j806krb.qnssl.com/public/images/cnodejs_light.svg"></a>
+      <ul class="nav clearfix">
+        <li><a href="#">首页</a></li>
+        <li><a href="#">新手入门</a></li>
+        <li><a href="#">API</a></li>
+        <li><a href="#">关于</a></li>
+        <li><a href="#">注册</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped>
+
+
+  .topNavBar {
+    background: #444444;
+    width:100%;
+  }
+
+  .container {
+    width:90%;
+    min-width: 960px;
+    max-width: 1400px;
+    margin:auto;
+    padding: 5px 0;
+  }
+  .container .logo{
+    display: inline-block;
+    height: 40px;
+  }
+  .container img{
+    height: 37px;
+    width: 134px;
+  }
+
+  .nav {
+    float: right;
+  }
+
+  .nav > li {
+    float: left;
+    color:#CCCCCC;
+    font-size: 13px;
+  }
+
+  .nav >li > a{
+    display:inline-block;
+    padding:12px 15px;
+  }
+
+  .clearfix:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+</style>
---
+++ src/components/Postlist.vue
@@ -0,0 +1,195 @@
+<template>
+  <ul class="postList">
+    <li class="nav">
+      <ul>
+        <li><a href="#">全部</a></li>
+        <li><a href="#">精华</a></li>
+        <li><a href="#">分享</a></li>
+        <li><a href="#">回答</a></li>
+        <li><a href="#">招聘</a></li>
+        <li><a href="#">客户端测试</a></li>
+      </ul>
+    </li>
+    <li v-for="post in postListData">
+      <a href="#" class="authorImg">
+        <img :src="post.author.avatar_url">
+      </a>
+      <span class="comment">
+        <span class="replyMount">{{post.reply_count}}</span><span class="count_seperator">/</span><span
+          class="clickMount">{{post.visit_count}}</span>
+      </span>
+      <span :class="[{active:isActive(post)},'topic']">{{getTopicContent(post)}}</span>
+      <a href="#" class="titileName">{{post.title}}</a>
+      <a href="" class="latestComment">{{post.last_reply_at | timeTransform}}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: "Postlist",
+    data() {
+      return {
+        postListData: '',
+        activeClass: ['top', 'good'],
+      }
+    },
+    methods: {
+      //获取cnode数据
+      getData() {
+        let _this = this
+        axios.get('https://cnodejs.org/api/v1/topics').then(response => {
+          _this.postListData = response.data.data
+          console.log(_this.postListData)
+        }).catch(error => {
+          console.log(error)
+        })
+      },
+      isActive(post) {
+        return this.activeClass.reduce((prev, curr) => {
+          return prev || post[curr]
+        }, false)
+      },
+      getTopicContent(post) {
+        if (post.top) {
+          return '置顶'
+        } else if (post.good) {
+          return '精华'
+        } else if (post.tab === 'share') {
+          return '分享'
+        } else if (post.tab === 'ask') {
+          return '问答'
+        }
+      },
+    },
+    computed: {},
+    filters: {
+      timeTransform(value) {
+        // 2018-08-11T11:31:51.799Z
+        let reg = /^(\d+)-(\d+)-(\d+)T(\d+):(\d+)/
+        let [, commentYear, commentMouth, commentDay, commentHour, commentMinute] = value.match(reg)
+        let currentDate = new Date()
+        let currentYear = currentDate.getFullYear()
+        let currentMonth = currentDate.getMonth() + 1
+        let currentDay = currentDate.getDate()
+        let currentHour = currentDate.getHours()
+        let currentMinute = currentDate.getMinutes()
+
+        if (currentYear != commentYear) {
+          return (currentYear - commentYear) + '年前'
+        } else if (currentMonth != commentMouth) {
+          return (currentMonth - commentMouth) + '月前'
+        } else if (currentDay != commentDay) {
+          return (currentDay - commentDay) + '天前'
+        } else if (commentHour != currentHour) {
+          return (currentHour - commentHour) + '小时前'
+        } else if (commentMinute != currentMinute) {
+          return (currentHour - commentHour) + '分钟前'
+        } else {
+          return '刚刚'
+        }
+      }
+    },
+    mounted() {
+      this.getData()
+    }
+  }
+</script>
+
+<style scoped>
+  .postList {
+    width: 80%;
+    margin: 0 auto;
+    background: white;
+  }
+
+  .postList > li:not(:first-child) {
+    border-bottom: 1px solid #F0F0F0;
+    padding: 10px;
+    line-height: 30px;
+    display: block;
+  }
+
+  .postList > li:not(:first-child):after {
+    content: '';
+    display: inline-block;
+    height: 100%;
+    vertical-align: middle;
+  }
+
+  .postList > li:not(:first-child):hover {
+    background: #F6F6F6;
+  }
+
+  .authorImg > img {
+    width: 30px;
+    height: 30px;
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .comment {
+    display: inline-block;
+    vertical-align: middle;
+    width: 70px;
+    text-align: center;
+  }
+
+  .count_seperator {
+    color: #333;
+    font-size: 12px;
+  }
+
+  .replyMount {
+    color: #9e78c0;
+    font-size: 14px;
+  }
+
+  .clickMount {
+    font-size: 12px;
+    color: #b4b4b4;
+
+  }
+
+  .topic {
+    padding: 2px 4px;
+    vertical-align: middle;
+    background: #E5E5E5;
+    color: #fff;
+    font-size: 12px;
+    border-radius: 3px;
+  }
+
+  .topic.active {
+    background: #80bd01;
+  }
+
+  .titileName {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .latestComment {
+    display: inline-block;
+    vertical-align: middle;
+    float: right;
+    color: #778087;
+    font-size: 11px;
+  }
+
+
+  .nav{
+    background: #F6F6F6;
+    display: block;
+  }
+  .nav li{
+    float:left;
+    color:#80bd01;
+    margin:0 10px;
+    font-size: 14px;
+
+  }
+
+</style>
